60d75591e9dbe45eb92dd07adda22e3a57419700
Sruthi <sruthiram44@gmail.com>
2015-05-31 23:41:38 -0500

    adding files to dist folder

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,7 @@
 /* jshint devel:true */
 /* smooth scrolling sections */
+'use strict';
+
 $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
         var target = $(this.hash);
